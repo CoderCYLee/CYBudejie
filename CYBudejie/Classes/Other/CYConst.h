41a5a5b61286fb2a35d3c9421f4cae5b934d8f83
Cyrill <lichunyang@outlook.com>
2016-12-07 11:57:06 +0800

    添加配置文件

--- CYBudejie/Classes/Other/CYConst.h
+++ CYBudejie/Classes/Other/CYConst.h
@@ -1,6 +1,14 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSUInteger, CYTopicsType) {
+    CYTopicsTypeAll = 1, // 全部
+    CYTopicsTypePicture = 10, // 图片
+    CYTopicsTypeWord = 29, // 段子
+    CYTopicsTypeVoice = 31, // 声音
+    CYTopicsTypeVideo = 41 // 视频
+};
+
 /** 精华-顶部标题的高度 */
 UIKIT_EXTERN CGFloat const CYTitilesViewH;
 /** 精华-顶部标题的Y */
@@ -12,3 +20,27 @@ UIKIT_EXTERN CGFloat const CYTopicCellMargin;
 UIKIT_EXTERN CGFloat const CYTopicCellTextY;
 /** 精华-cell-底部工具条的高度 */
 UIKIT_EXTERN CGFloat const CYTopicCellBottomBarH;
+
+/** 精华-cell-图片帖子的最大高度 */
+UIKIT_EXTERN CGFloat const CYTopicCellPictureMaxH;
+/** 精华-cell-图片帖子一旦超过最大高度,就是用Break */
+UIKIT_EXTERN CGFloat const CYTopicCellPictureBreakH;
+
+/** CYUser模型-性别属性值 */
+UIKIT_EXTERN NSString * const CYUserSexMale;
+UIKIT_EXTERN NSString * const CYUserSexFemale;
+
+/** 精华-cell-最热评论标题的高度 */
+UIKIT_EXTERN CGFloat const CYTopicCellTopCmtTitleH;
+
+/** tabBar被选中的通知名字 */
+UIKIT_EXTERN NSString * const CYTabBarDidSelectNotification;
+/** tabBar被选中的通知 - 被选中的控制器的index key */
+UIKIT_EXTERN NSString * const CYSelectedControllerIndexKey;
+/** tabBar被选中的通知 - 被选中的控制器 key */
+UIKIT_EXTERN NSString * const CYSelectedControllerKey;
+
+/** 标签-间距 */
+UIKIT_EXTERN CGFloat const CYTagMargin;
+/** 标签-高度 */
+UIKIT_EXTERN CGFloat const CYTagH;
--- CYBudejie/Classes/Other/CYConst.m
+++ CYBudejie/Classes/Other/CYConst.m
@@ -2,15 +2,40 @@
 #import <UIKit/UIKit.h>
 
 /** 精华-顶部标题的高度 */
-CGFloat const XMGTitilesViewH = 35;
+CGFloat const CYTitilesViewH = 35;
 /** 精华-顶部标题的Y */
-CGFloat const XMGTitilesViewY = 64;
+CGFloat const CYTitilesViewY = 64;
 
 /** 精华-cell-间距 */
-CGFloat const XMGTopicCellMargin = 10;
+CGFloat const CYTopicCellMargin = 10;
 /** 精华-cell-文字内容的Y值 */
-CGFloat const XMGTopicCellTextY = 55;
+CGFloat const CYTopicCellTextY = 55;
 /** 精华-cell-底部工具条的高度 */
-CGFloat const XMGTopicCellBottomBarH = 44;
+CGFloat const CYTopicCellBottomBarH = 35;
+
+/** 精华-cell-图片帖子的最大高度 */
+CGFloat const CYTopicCellPictureMaxH = 1000;
+/** 精华-cell-图片帖子一旦超过最大高度,就是用Break */
+CGFloat const CYTopicCellPictureBreakH = 250;
+
+/** CYUser模型-性别属性值 */
+NSString * const CYUserSexMale = @"m";
+NSString * const CYUserSexFemale = @"f";
+
+/** 精华-cell-最热评论标题的高度 */
+CGFloat const CYTopicCellTopCmtTitleH = 20;
+
+/** tabBar被选中的通知名字 */
+NSString * const CYTabBarDidSelectNotification = @"CYTabBarDidSelectNotification";
+/** tabBar被选中的通知 - 被选中的控制器的index key */
+NSString * const CYSelectedControllerIndexKey = @"CYSelectedControllerIndexKey";
+/** tabBar被选中的通知 - 被选中的控制器 key */
+NSString * const CYSelectedControllerKey = @"CYSelectedControllerKey";
+
+/** 标签-间距 */
+CGFloat const CYTagMargin = 5;
+/** 标签-高度 */
+CGFloat const CYTagH = 25;
+
 
 
